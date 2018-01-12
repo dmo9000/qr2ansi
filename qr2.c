b1cc30b4562658eed0d0fe5bf6f3a8ad0bb80ffd
Dan Morriss <daniel_morriss@yahoo.com.au>
2018-01-12 16:50:48 +1100

    Remove redundant comment which is duplicated in README. Some fixes to make splint happy.

--- qr2.c
+++ qr2.c
@@ -1,24 +1,5 @@
-/* Inspired by and adapted from: 
-
-	  https://gist.github.com/michaelschrijver/8566294
-
-   That version is great, but I needed a version that used CP437 half blocks 
-   blocks in order to make the code fit more easily on an 80x24 terminal 
-
-	 Compile with: 
-
- 		gcc -o qr2 qr2.c -lqrencode
-
-   You will need to pipe the output through iconv for now to get UTF-8 
-	 compatible output, eg.
-
-	 ./qr2 "foobarbaz.com" | iconv -f CP437 -t UTF-8
-
-   Since the output of this program is in obsolete CP437 format. 
-
-*/
-
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <assert.h>
@@ -27,30 +8,30 @@
 #define DEFAULT_WIDTH		79	
 #define DEFAULT_HEIGHT	23
 
-uint8_t tty_width;
-uint8_t tty_height;
+static uint8_t tty_width;
+static uint8_t tty_height;
 
-static const int border = 4;
+static const uint8_t border = 4;
 
 static void put_block (bool upper, bool lower) {
 
     if (upper && lower) {
-        putchar(0xdb);
+        (void) putchar(0xdb);
         return;
     }
 
     if (upper && !lower) {
-        putchar(0xdf);
+        (void) putchar(0xdf);
         return;
     }
 
     if (lower && !upper) {
-        putchar(0xdc);
+        (void) putchar(0xdc);
         return;
     }
 
     if (!lower && !upper) {
-        putchar(0x20);
+        (void) putchar(0x20);
         return;
     }
 
@@ -64,14 +45,14 @@ static void horizontal_border(uint8_t my_padding) {
 				}
 }
 
-static void vertical_border(int my_width) {
+static void vertical_border(uint8_t my_width) {
     uint8_t x, y;
 
     for (y = 0 ; y < border ; y++) {
         for (x = 0 ; x < my_width ; x++) {
             put_block(true, true);
 					}
-        putchar('\n');
+        (void) putchar('\n');
     }
 }
 
@@ -85,20 +66,20 @@ static void put_qrcode (const char *str) {
 		tty_width = DEFAULT_WIDTH;
 		tty_height = DEFAULT_HEIGHT;
 
-		padding = (tty_width - qr->width) / 2; 
+		padding = (tty_width - (uint8_t) qr->width) / 2; 
 		vertical_border(tty_width);
-    for (y = 0 ; y < qr->width ; y+=2) {
+    for (y = 0 ; y < (uint8_t) qr->width ; y+=2) {
         horizontal_border (padding);
-        for (x = 0 ; x < qr->width ; x++) {
-            upper = !(qr->data[y * qr->width + x] & 1);
-            lower = !(qr->data[(y+1) * qr->width + x] & 1);
-            if (y + 1 >= qr->width) {
+        for (x = 0 ; x < (uint8_t) qr->width ; x++) {
+            upper = !((bool) (qr->data[y * (uint8_t) qr->width + x] & 1));
+            lower = !((bool) (qr->data[(y+1) * (uint8_t) qr->width + x] & 1));
+            if (y + 1 >= (uint8_t) qr->width) {
                 lower = true;
             }
             put_block(upper, lower);
         }
         horizontal_border (padding);
-        putchar('\n');
+        (void) putchar('\n');
     }
 		vertical_border(tty_width);
 
@@ -107,7 +88,9 @@ static void put_qrcode (const char *str) {
 
 int main (int argc, char *argv[]) {
     uint8_t i;
-    for (i = 1 ; i < argc ; i++) {
+    for (i = 1 ; i < (uint8_t) argc ; i++) {
         put_qrcode (argv[i]);
     }
+
+    exit(0);
 }
